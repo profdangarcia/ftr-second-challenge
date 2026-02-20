1c07780b3b70e66f4545eaa603d0936f8bbad1d9
Dan Garcia <daniel.of@gmail.com>
2026-02-20 11:50:05 -0300

    feat: badge

---
+++ frontend/src/components/ui/badge.tsx
@@ -0,0 +1,47 @@
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const badgeVariants = cva(
+  "inline-flex items-center justify-center rounded-full border border-transparent px-3 py-1 text-sm w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden",
+  {
+    variants: {
+      variant: {
+        default: "bg-gray-200 text-gray-700",
+        blue: "bg-blue-light text-blue-dark",
+        purple: "bg-purple-light text-purple-dark",
+        pink: "bg-pink-light text-pink-dark",
+        red: "bg-red-light text-red-dark",
+        orange: "bg-orange-light text-orange-dark",
+        yellow: "bg-yellow-light text-yellow-dark",
+        green: "bg-green-light text-green-dark",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+function Badge({
+  className,
+  variant = "default",
+  asChild = false,
+  ...props
+}: React.ComponentProps<"span"> &
+  VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot : "span"
+
+  return (
+    <Comp
+      data-slot="badge"
+      data-variant={variant}
+      className={cn(badgeVariants({ variant }), className)}
+      {...props}
+    />
+  )
+}
+
+export { Badge, badgeVariants }
