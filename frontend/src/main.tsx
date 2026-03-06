76d24045d48f1e2b1b0214c510392525df1c10c1
Dan Garcia <daniel.of@gmail.com>
2026-03-06 18:28:11 -0300

    fix: app router and process kill

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,16 +2,19 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, HashRouter } from "react-router-dom"
 import { ApolloProvider } from "@apollo/client/react"
 import { apolloClient } from "./lib/graphql/apollo.ts"
+
+const isFileProtocol = window.location.protocol === "file:"
+const Router = isFileProtocol ? HashRouter : BrowserRouter
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ApolloProvider client={apolloClient}>
-      <BrowserRouter>
+      <Router>
         <App />
-      </BrowserRouter>
+      </Router>
     </ApolloProvider>
   </StrictMode>
 )
